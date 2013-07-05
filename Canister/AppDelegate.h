b7b76d7ff1adf2485a9cf391a72a35c07e783b5c
Scott James <Scott@Scotts-MacBook-Air.local>
2013-07-05 15:42:35 -0400

    Initial Commit

---
+++ Canister/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Canister
+//
+//  Created by Scott James on 7/5/13.
+//  Copyright (c) 2013 Scott James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Canister/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Canister
+//
+//  Created by Scott James on 7/5/13.
+//  Copyright (c) 2013 Scott James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
